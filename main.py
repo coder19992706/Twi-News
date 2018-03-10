d45a91c250c9e087af5c4169bbf2ff8cfd66e548
NIKHIL <nikhil.accent@gmail.com>
2018-03-10 01:35:41 +0000

    changed main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from flask import Flask
-from flask import render_template
+from flask_session import Session
+from flask import Flask, flash, redirect, render_template, request, session, url_for
 app = Flask(__name__)
 
 @app.route('/')
